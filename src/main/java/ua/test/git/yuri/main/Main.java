c0e4cc53192558c7ad137f4473a8a29329a68f8c
Yuri100500 <flashback100500@gmail.com>
2014-12-18 10:11:22 +0200

    first commit trololo

---
+++ src/main/java/ua/test/git/yuri/main/Main.java
@@ -0,0 +1,10 @@
+package ua.test.git.yuri.main;
+
+/**
+ * Created by Iurii_Galias on 12/17/14.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
